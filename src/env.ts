e65b8a7752c1980eb75153e27004817add6ccb22
KATT <alexander@n1s.se>
2019-06-08 21:10:11 +0200

    🚀 Deploy to now

--- src/env.ts
+++ src/env.ts
@@ -1,11 +1,6 @@
 import { cleanEnv, num, str } from 'envalid';
-import { config } from 'dotenv'
 
-config()
 export const createEnv = (env: NodeJS.ProcessEnv = process.env) => cleanEnv(env, {
-  PORT: num({
-    default: 24736,
-  }),
   TOGGL_API_TOKEN: str()
 }, {
     strict: true,
