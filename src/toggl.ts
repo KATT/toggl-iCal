65c2b826ba89973be2abd75543bf3478b7c104c6
KATT <alexander@n1s.se>
2019-06-10 01:19:19 +0200

    🎨 Make Toggl client reusable for different tokens

--- src/toggl.ts
+++ src/toggl.ts
@@ -2,22 +2,6 @@ import { env } from './env';
 import Axios from 'axios';
 import { stringify } from 'querystring';
 
-const { TOGGL_API_TOKEN } = env
-
-const togglAxios = Axios.create({
-	baseURL: 'https://www.toggl.com/api/v8',
-	auth: {
-		username: TOGGL_API_TOKEN,
-		password: 'api_token'
-	},
-})
-
-async function get<T>(path: string): Promise<T> {
-	const { data } = await togglAxios(path)
-
-	return data;
-}
-
 
 export interface TogglEntry {
 	id: number;
@@ -53,34 +37,56 @@ export interface TogglProject {
 }
 
 
-export async function getEntries({ start_date, end_date }: { start_date: Date, end_date: Date }) {
-	const query: any = {
-		start_date: start_date.toJSON(),
-		end_date: end_date.toJSON()
+export function createClient({ token }: { token: string }) {
+	const togglAxios = Axios.create({
+		baseURL: 'https://www.toggl.com/api/v8',
+		auth: {
+			username: token,
+			password: 'api_token'
+		},
+	})
+
+	async function get<T>(path: string): Promise<T> {
+		const { data } = await togglAxios(path)
+
+		return data;
 	}
 
-	const path = `/time_entries?${stringify(query)}`
-
-	return get<TogglEntry[]>(path)
-}
-
-export async function getProjectById(id: number) {
-	const path = `/projects/${id}`
-
-	const data = await get<{ data: TogglProject }>(path)
-
-	return data.data
-}
-
-export function projectByIdLoaderFactory() {
-	const cache = new Map<number, Promise<TogglProject>>()
-
-	return async function load(id: number) {
-		if (!cache.has(id)) {
-			const promise = getProjectById(id)
-			cache.set(id, promise)
+	async function getEntries({ start_date, end_date }: { start_date: Date, end_date: Date }) {
+		const query: any = {
+			start_date: start_date.toJSON(),
+			end_date: end_date.toJSON()
 		}
 
-		return cache.get(id)!
+		const path = `/time_entries?${stringify(query)}`
+
+		return get<TogglEntry[]>(path)
+	}
+
+	async function getProjectById(id: number) {
+		const path = `/projects/${id}`
+
+		const data = await get<{ data: TogglProject }>(path)
+
+		return data.data
+	}
+
+	function projectByIdLoaderFactory() {
+		const cache = new Map<number, Promise<TogglProject>>()
+
+		return async function load(id: number) {
+			if (!cache.has(id)) {
+				const promise = getProjectById(id)
+				cache.set(id, promise)
+			}
+
+			return cache.get(id)!
+		}
+	}
+
+	return {
+		getProjectById,
+		projectByIdLoaderFactory,
+		getEntries,
 	}
 }
