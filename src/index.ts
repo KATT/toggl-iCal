6f82fb2705b2ec73affb217dfc7f10a2adff40d8
KATT <alexander@n1s.se>
2019-06-10 11:22:40 +0200

    🎨 Fix formatting

--- src/index.ts
+++ src/index.ts
@@ -1,64 +1,68 @@
-import { createClient } from './toggl';
-import ical from 'ical-generator';
-import { IncomingMessage, ServerResponse } from 'http';
-import moment from 'moment';
-import url from 'url';
-import querystring from 'querystring'
+import { createClient } from "./toggl";
+import ical from "ical-generator";
+import { IncomingMessage, ServerResponse } from "http";
+import moment from "moment";
+import url from "url";
+import querystring from "querystring";
 
 async function getCal({ token }: { token: string }) {
-	const toggl = createClient({ token })
+  const toggl = createClient({ token });
 
-	const loadProjectById = toggl.projectByIdLoaderFactory()
+  const loadProjectById = toggl.projectByIdLoaderFactory();
 
-	const cal = ical({
-		name: 'Toggl time entries',
-		domain: 'kattcorp.com',
-	});
+  const cal = ical({
+    name: "Toggl time entries",
+    domain: "kattcorp.com",
+  });
 
-	const entries = await toggl.getEntries({
-		start_date: moment().subtract(6, 'months').toDate(),
-		end_date: moment().toDate()
-	})
+  const entries = await toggl.getEntries({
+    start_date: moment()
+      .subtract(6, "months")
+      .toDate(),
+    end_date: moment().toDate(),
+  });
 
-	const entriesWithProjects = await Promise.all(
-		entries.map(async (entry) => {
-			const project = await loadProjectById(entry.pid)
+  const entriesWithProjects = await Promise.all(
+    entries.map(async entry => {
+      const project = await loadProjectById(entry.pid);
 
-			return {
-				...entry,
-				project,
-			}
-		})
-	)
+      return {
+        ...entry,
+        project,
+      };
+    }),
+  );
 
-	for (const entry of entriesWithProjects) {
-		const icon = entry.billable ? '💲' : '❌'
+  for (const entry of entriesWithProjects) {
+    const icon = entry.billable ? "💲" : "❌";
 
-		const durationInHoursRounded = Math.round((entry.duration / 60 / 60) * 10) / 10
-		const duration = durationInHoursRounded > 0 ? `${durationInHoursRounded}h` : 'n/a'
+    const durationInHoursRounded =
+      Math.round((entry.duration / 60 / 60) * 10) / 10;
+    const duration =
+      durationInHoursRounded > 0 ? `${durationInHoursRounded}h` : "n/a";
 
-		cal.createEvent({
-			start: moment(entry.start),
-			end: moment(entry.stop),
-			summary: `${icon} ${entry.project.name}: ${entry.description} - ${duration} ⏳`
-		})
-	}
+    cal.createEvent({
+      start: moment(entry.start),
+      end: moment(entry.stop),
+      summary: `${icon} ${entry.project.name}: ${
+        entry.description
+      } - ${duration} ⏳`,
+    });
+  }
 
-	return cal
+  return cal;
 }
 
 export default async (req: IncomingMessage, res: ServerResponse) => {
-	const parts = url.parse(req.url!);
-	const { token } = querystring.parse(parts.query || '');
+  const parts = url.parse(req.url!);
+  const { token } = querystring.parse(parts.query || "");
 
-	if (typeof token !== 'string') {
-		res.writeHead(400);
-		res.end(
-			'Missing query parameter "token"'
-		);
-		return
-	}
+  if (typeof token !== "string") {
+    res.writeHead(400);
+    res.end('Missing query parameter "token"');
+    return;
+  }
 
-	const cal = await getCal({ token })
-	cal.serve(res)
+  const cal = await getCal({ token });
+  cal.serve(res);
 };
--- src/toggl.ts
+++ src/toggl.ts
@@ -1,91 +1,95 @@
-import Axios from 'axios';
-import qs from 'querystring';
-
+import Axios from "axios";
+import qs from "querystring";
 
 export interface TogglEntry {
-	id: number;
-	guid: string;
-	wid: number;
-	pid: number;
-	billable: boolean;
-	start: Date;
-	stop: Date;
-	duration: number;
-	description: string;
-	duronly: boolean;
-	at: Date;
-	uid: number;
+  id: number;
+  guid: string;
+  wid: number;
+  pid: number;
+  billable: boolean;
+  start: Date;
+  stop: Date;
+  duration: number;
+  description: string;
+  duronly: boolean;
+  at: Date;
+  uid: number;
 }
 
 export interface TogglProject {
-	id: number;
-	wid: number;
-	cid: number;
-	name: string;
-	billable: boolean;
-	is_private: boolean;
-	active: boolean;
-	template: boolean;
-	at: Date;
-	created_at: Date;
-	color: string;
-	auto_estimates: boolean;
-	actual_hours: number;
-	hex_color: string;
-	rate?: number;
+  id: number;
+  wid: number;
+  cid: number;
+  name: string;
+  billable: boolean;
+  is_private: boolean;
+  active: boolean;
+  template: boolean;
+  at: Date;
+  created_at: Date;
+  color: string;
+  auto_estimates: boolean;
+  actual_hours: number;
+  hex_color: string;
+  rate?: number;
 }
 
-
 export function createClient({ token }: { token: string }) {
-	const togglAxios = Axios.create({
-		baseURL: 'https://www.toggl.com/api/v8',
-		auth: {
-			username: token,
-			password: 'api_token'
-		},
-	})
+  const togglAxios = Axios.create({
+    baseURL: "https://www.toggl.com/api/v8",
+    auth: {
+      username: token,
+      password: "api_token",
+    },
+  });
 
-	async function get<T>(path: string): Promise<T> {
-		const { data } = await togglAxios(path)
+  async function get<T>(path: string): Promise<T> {
+    const { data } = await togglAxios(path);
 
-		return data;
-	}
+    return data;
+  }
 
-	async function getEntries({ start_date, end_date }: { start_date: Date, end_date: Date }) {
-		const query: any = {
-			start_date: start_date.toJSON(),
-			end_date: end_date.toJSON()
-		}
+  async function getEntries({
+    start_date,
+    end_date,
+  }: {
+    start_date: Date;
+    end_date: Date;
+  }) {
+    const query: any = {
+      start_date: start_date.toJSON(),
+      end_date: end_date.toJSON(),
+    };
 
-		const path = `/time_entries?${qs.stringify(query)}`
+    const path = `/time_entries?${qs.stringify(query)}`;
 
-		return get<TogglEntry[]>(path)
-	}
+    return get<TogglEntry[]>(path);
+  }
 
-	async function getProjectById(id: number) {
-		const path = `/projects/${id}`
+  async function getProjectById(id: number) {
+    const path = `/projects/${id}`;
 
-		const data = await get<{ data: TogglProject }>(path)
+    const data = await get<{ data: TogglProject }>(path);
 
-		return data.data
-	}
+    return data.data;
+  }
 
-	function projectByIdLoaderFactory() {
-		const cache = new Map<number, Promise<TogglProject>>()
+  function projectByIdLoaderFactory() {
+    const cache = new Map<number, Promise<TogglProject>>();
 
-		return async function load(id: number) {
-			if (!cache.has(id)) {
-				const promise = getProjectById(id)
-				cache.set(id, promise)
-			}
+    return async function load(id: number) {
+      if (!cache.has(id)) {
+        const promise = getProjectById(id);
+        cache.set(id, promise);
+      }
 
-			return cache.get(id)!
-		}
-	}
+      return cache.get(id)!;
+    };
+  }
 
-	return {
-		getProjectById,
-		projectByIdLoaderFactory,
-		getEntries,
-	}
+  return {
+    getProjectById,
+    projectByIdLoaderFactory,
+    getEntries,
+  };
 }
